f5f66e471f89585802d1ea17faf143a704d9a22d
bhupendra.aole@gmail.com <bhupendra.aole@gmail.com>
2018-04-27 16:53:42 -0400

    Line selection/deletion. Scroll bars

--- facemap.py
+++ facemap.py
@@ -2,24 +2,20 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import Image, ImageTk
 from tkinter import messagebox
+from tkinter import ttk
+
 ''' 
 TODO:
 1. edit mirror
-2. selected handle/line
-3. delete lines
-4. undo/redo
 5. truncate lines at mirror
 6. create mirrored lines; ctrl+m on selected line
-7. only 1 editable line
-8. F1 (Help screen)
-9. resize
+10. select multiple
+11. Panning
+12. other shapes
+13. edit only selected lines
+14. scroll bars
+15.
 '''
-
-help_msg = 'Tab: toggle create/edit mode.\n'
-help_msg += 'Ctrl+i: open image file to load as background.\n'
-help_msg += 'Ctrl+o: open stored lines file.\n'
-help_msg += 'Ctrl+s: save lines to a file.\n'
-help_msg += 'm: show mirror line.\n'
 
 class Handle:
     count = 1
@@ -56,7 +52,7 @@ class Handle:
         
         self.mousedown = False
         
-        self.shape = self.canvas.create_rectangle( x-self.w2, y-self.h2, x+self.w2, y+self.h2, activefill='#BBBBEE' )
+        self.shape = self.canvas.create_rectangle( x-self.w2, y-self.h2, x+self.w2, y+self.h2, activeoutline='#BBBBEE' )
         
         self.canvas.tag_bind( self.shape, '<Button-1>', self.on_left_click )
         self.canvas.tag_bind( self.shape, '<ButtonRelease-1>', self.on_left_up )
@@ -168,10 +164,7 @@ class Line:
         line_coords.insert( index*2, x )
         self.canvas.coords( self.shape, line_coords )
         
-        if app.is_edit_mode():
-            handle.show()
-        else:
-            handle.hide()
+        handle.show()
         
     def remove_handle( self, handle ):
         if len(self.handles)<=2:
@@ -188,7 +181,7 @@ class Line:
         for idx in range( index, len(self.handles) ):
             self.handles[idx].index -= 1
         
-        handle.hide()
+        handle.show()
         
     def update_end( self, x, y ):
         self.handles[-1].update( x, y )
@@ -220,6 +213,12 @@ class Line:
     def is_visible( self ):
         return self.visible
         
+    def select( self ):
+        self.canvas.itemconfig( self.shape, fill='blue' )
+    
+    def deselect( self ):
+        self.canvas.itemconfig( self.shape, fill='black' )
+        
 class UndoManager:
     def __init__(self,):
         self.undos = []
@@ -245,59 +244,90 @@ class UndoManager:
         except IndexError:
             pass
 
-class Application:
-    lines = [] # all lines that need drawing
+class Application( tk.Frame ):
     
     mousedown = False #is mouse button down
     prevx, prevy = 0,0 # last mouse moved position. needed for dx/dy
     
-    selectedline = None
-    selectedhandle = None # handle that is currently editing
-    
     mousemode = 1 #Create. 2: Edit. press TAB to change
     
-    fpx,fpy = 0,0
-    secondpoint = False
-    
-    showmirror = False
-    
+    class SelectAction:
+        def __init__( self, app, newSelection, oldSelection ):
+            self.app = app
+            self.newSelection = newSelection
+            self.oldSelection = oldSelection
+            
+        def undo(self):
+            self.app.select_line( self.oldSelection, False )
+            
+        def redo(self):
+            self.app.select_line( self.newSelection, False )
+            
+    class DeleteAction:
+        def __init__( self, line ):
+            self.line = line
+            
+        def undo(self):
+            self.line.set_visible( True )
+            
+        def redo(self):
+            self.line.set_visible( False )
+            
     def __init__(self, w, h):
+        mousemode = 1
+        self.lines = [] # all lines that need drawing
+        self.linesMap = {}
+        self.currentLine = None
+        self.selectedLine = None
+        
         self.undomanager = UndoManager()
         
         self.root = tk.Tk()
         self.root.title("Face Map")
 
-        self.canvas = tk.Canvas(self.root, width=w, height=h, closeenough=5)
+        tk.Frame.__init__( self, self.root )
+        
+        self.canvas = tk.Canvas( self, closeenough=5 )
+        
         self.canvas.image = None
         self.image_shape = None
-
-        self.canvas.bind('<ButtonPress-1>', self.left_mouse_down) #left mouse down
-        self.canvas.bind('<ButtonRelease-1>', self.left_mouse_up) #left mouse up
-        self.canvas.bind('<Motion>', self.mouse_move) #move move
-        self.canvas.bind('<Key-Tab>', self.key_tab) #move move
-        self.canvas.bind('<Key-m>', self.key_m) #move move
-        self.canvas.bind('<Enter>', self.mouse_enter)
-        self.root.bind('<Control-Key-i>', self.key_control_i)
-        self.root.bind('<Key-i>', self.key_i)
-        self.root.bind('<Control-Key-o>', self.key_control_o)
-        self.root.bind('<Control-Key-s>', self.key_control_s)
-        self.root.bind('<Control-Key-z>', self.key_control_z)
-        self.root.bind('<Control-Shift-Key-Z>', self.key_control_y)
-        self.root.bind('<Key-F1>', self.key_F1)
-        self.canvas.pack()
+        
+        vbar = ttk.Scrollbar( self, orient='vertical', command=self.canvas.yview )
+        hbar = ttk.Scrollbar( self, orient='horizontal', command=self.canvas.xview )
+        self.canvas.configure( yscrollcommand=vbar.set, xscrollcommand=hbar.set )
+        self.canvas.configure( scrollregion=(-500,-500,500,500) )
+        
+        hbar.grid(row=1, column=0, sticky="ew")
+        vbar.grid(row=0, column=1, sticky="ns")
+        self.canvas.grid(row=0, column=0, sticky='nswe')
+        self.grid_rowconfigure( 0, weight=1 )
+        self.grid_columnconfigure( 0, weight=1 )
+        
+        #self.root.bind( '<Configure>', self.resize )
+        self.root.bind( '<Control-Key-i>', self.key_control_i )
+        self.root.bind( '<Key-i>', self.key_i )
+        self.root.bind( '<Control-Key-o>', self.key_control_o ) 
+        self.root.bind( '<Control-Key-s>', self.key_control_s )
+        self.root.bind( '<Control-Key-z>', self.key_control_z )
+        self.root.bind( '<Control-Shift-Key-Z>', self.key_control_y )
+        self.root.bind( '<Key-F1>', self.key_F1 )
+        
+        self.canvas.bind( '<ButtonPress-1>', self.left_mouse_down ) #left mouse down
+        self.canvas.bind( '<ButtonRelease-1>', self.left_mouse_up ) #left mouse up
+        self.canvas.bind( '<Motion>', self.mouse_move )
+        self.canvas.bind( '<Key-Tab>', self.key_tab )
+        self.canvas.bind( '<Key-Delete>', self.key_delete )
+        self.canvas.bind( '<Enter>', self.mouse_enter )
+        
+        #self.canvas.pack( fill=tk.BOTH, expand=1 )
 
         self.display_help_text()
         
-        mousemode = 1
-        self.currentLine = None
-        #self.root.config(cursor="target")
-        
-        #self.label = tk.Label(self.root, anchor=tk.S, text='coords')
-        #self.label.pack()
-
     def display_help_text( self ):
         txts = ['Press F1 to toggle help text.',
             'Drag left mouse button to create / edit lines.',
+            'Click on lines to select them.',
+            'Press DEL to delete selected lines.',
             'Press TAB to toggle line creation / editing.',
             'Press CTRL+z, CTRL+SHIFT+z for undo and redo.',
             'Press CTRL+o, CTRL+s to open and save file.',
@@ -309,6 +339,7 @@ class Application:
         for i in range(len(txts)):
             ht = self.canvas.create_text( ( 10, i*20 + 10 ), text=txts[i], fill='gray', anchor='nw' )
             self.helptext.append( ht )
+            
         self.helpdisplayed = True
         
     def key_F1( self, event ):
@@ -323,29 +354,62 @@ class Application:
             
     def left_mouse_down( self, event ):
         self.mousedown = True
-        if self.mousemode==1:
-            line = Line( self, [event.x, event.y, event.x, event.y] )
-            self.currentLine = line
-            
         self.prevx, self.prevy = self.origx, self.origy = event.x, event.y
     
     def mouse_move( self, event ):
-        #self.label['text'] = str(event.x)+','+str(event.y)
         if self.mousemode==1:
             if self.currentLine:
                 self.currentLine.update_end( event.x, event.y )
+            elif self.mousedown:
+                line = Line( self, [self.origx, self.origy, event.x, event.y] )
+                self.currentLine = line
                 
         self.prevx, self.prevy = event.x, event.y
     
     def left_mouse_up( self, event ):
         self.mousedown = False
-        if self.mousemode==1:
-            self.lines.append( self.currentLine )
-            self.undomanager.add( Line.LineAddAction( self.currentLine ) )
+        if self.origx==event.x and self.origy==event.y:
+            lineshape = self.canvas.find_withtag( tk.CURRENT )
+            try:
+                line = self.linesMap[lineshape[0]]
+            except:
+                line = None
+            self.select_line( line )
+        else:
+            if self.mousemode==1:
+                self.add_line( self.currentLine )
     
-        self.currentLine = None
+            self.currentLine = None
+            
         self.prevx, self.prevy = event.x, event.y
 
+    def add_line( self, line, fromFile = False ):
+        self.lines.append( line )
+        self.linesMap[line.shape] = line
+        if not fromFile:
+            self.undomanager.add( Line.LineAddAction( line ) )
+            self.select_line( line )
+        
+    def select_line( self, line, undoable=True ):
+        if self.selectedLine:
+            self.selectedLine.deselect()
+            
+        if undoable:
+            self.undomanager.add( Application.SelectAction( self, line, self.selectedLine ) )
+            
+        self.selectedLine = line
+        if line:
+            line.select()
+        
+    def key_delete( self, event ):
+        if self.selectedLine:
+            self.delete_line( self.selectedLine )
+        
+    def delete_line( self, line ):
+        self.selectedLine.set_visible( False )
+        self.undomanager.add( Application.DeleteAction( self.selectedLine ) )
+        self.select_line( None, False )
+        
     def key_tab( self, event ):
         self.toggle_mouse_mode()
     
@@ -360,9 +424,6 @@ class Application:
     def run(self):
         self.root.mainloop()
 
-    def key_m( self, event ):
-        self.showmirror = not self.showmirror
-    
     def key_control_s( self, event ):
         #filename = asksaveasfilename(defaultextension='.txt')
         filename = 'test.txt'
@@ -370,11 +431,6 @@ class Application:
             file = open(filename, 'w')
             self.save(file)
             file.close()
-    
-    def save(self, file):
-        for line in self.lines:
-            line.save(file)
-            file.write('\n')
     
     def key_control_o( self, event ):
         #filename = askopenfilename()
@@ -384,27 +440,26 @@ class Application:
             self.load(file)
             file.close()
     
+    def save(self, file):
+        for line in self.lines:
+            if line.visible:
+                line.save(file)
+                file.write('\n')
+    
     def load(self, file):
         self.lines.clear()
+        self.linesMap.clear()
         for readline in file:
             if readline.strip()=='':
                 continue
             line = Line.load( self, readline.strip() )
-            self.lines.append( line )
+            self.add_line( line, fromFile = True )
             
     def key_control_i(self, event):
         # filename = askopenfilename()
         filename = 'facemap.jpg'
         if filename != None:
             self.render_image(filename, (0,0))
-    
-    def render_image(self, file, loc):
-        image = Image.open(file)
-        photo = ImageTk.PhotoImage(image)
-        self.canvas.image = photo
-        self.image_shape = self.canvas.create_image(loc, image = photo, anchor = tk.NW)
-        self.canvas.tag_lower( self.image_shape ) # Moves to the bottom of the canvas stack.
-        self.image_displayed = True
     
     def key_i(self, event):
         if self.image_shape:
@@ -415,6 +470,14 @@ class Application:
                 self.canvas.itemconfig( self.image_shape, state=tk.NORMAL )
                 self.image_displayed = True
         
+    def render_image(self, file, loc):
+        image = Image.open(file)
+        photo = ImageTk.PhotoImage(image)
+        self.canvas.image = photo
+        self.image_shape = self.canvas.create_image(loc, image = photo, anchor = tk.NW)
+        self.canvas.tag_lower( self.image_shape ) # Moves to the bottom of the canvas stack.
+        self.image_displayed = True
+    
     # Undo management
     def key_control_z( self, event ):
         self.undomanager.undo()
@@ -426,5 +489,6 @@ class Application:
         return self.mousemode == 2
         
 app = Application(480, 480)
+app.pack(fill="both", expand=True)
 app.load( open('test.txt', 'r') )
 app.run()
